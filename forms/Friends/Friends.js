ef58e5a8295cf9989453218a066a335d1a992730
freeyungcoop <50121590+freeyungcoop@users.noreply.github.com>
2020-02-12 15:13:19 -0600

    fixit 7
    fixit plus debug

---
+++ forms/Friends/Friends.js
@@ -0,0 +1,19 @@
+let firstFriend = prompt("Enter the name of your first of three friends.")
+let secondFriend = prompt("Enter the name of your second of three friends.")
+let thirdFriend = prompt("Enter the name of your third of three friends.")
+let bestFriendNumber = Number(prompt("Enter a 1 if your first person was your best friend, a 2 for the second, and a 3 for the third"))
+
+var friendNames = []
+
+friendNames[1] = firstFriend
+friendNames[2] = secondFriend
+friendNames[3] = thirdFriend
+let bestFriend = friendNames[bestFriendNumber]
+
+if (bestFriend.charAt(0) == "A") {
+   alert("Your best friend's name starts with A.")
+} else if (bestFriend.charAt(0) == "B") {
+   alert("Your best friend's name starts with B.")
+} else {
+   alert("Your best friend's name starts with something other than A or B.")
+}
--- forms/String/String.js
+++ forms/String/String.js
@@ -1,13 +1,23 @@
+//Using a string method, change the text in quoteString to all upper case and save it in a new variable named upperCaseString. Use this format for your output:
 let quoteString = "Failure is simply the opportunity to begin again, this time more intelligently."
 let upperCaseString = quoteString.toUpperCase()
 console.log(upperCaseString)
 
+//Create a new variable named authorString and put this text in it. 
 let authorString = " -Henry Ford"
+
+//Then create a new variable completeString that holds a new string created by using a string method to put the string in authorString on the end of the string in quoteString.
 let completeString = quoteString.concat(authorString)
+console.log(`The string in quoteString is: ${completeString}`)
+
+//Now create a new string variable named secondQuote and put this string in it
 let secondQuote = "Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. -Stephen King"
+
+//Using a string method, change the text in secondQuote to all lower case and save it in a new variable named lowerCaseString. 
 let lowerCaseString = secondQuote.toLowerCase()
-console.log(lowerCaseString)
-console.log(secondQuote.charAt(2))
+console.log(`lower case string is: ${lowerCaseString}`)
+
+console.log(`The character at location 3 is ${secondQuote.charAt(2)}`)
 
 let findString = secondQuote.slice(35,120)
 console.log(findString)
